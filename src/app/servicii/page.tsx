2fee0e828e4efe39204803b82b756477297b39e7
gorillaz1995 <104136116+gorillaz1995@users.noreply.github.com>
2024-11-28 15:48:46 +0200

    faust2

--- src/app/servicii/page.tsx
+++ src/app/servicii/page.tsx
@@ -4,7 +4,7 @@ import HailMary from "../components/graces/HailMary";
 function page() {
   return (
     <div>
-      <div className="w-full min-h-screen bg-[rgba(15,15,25,1)] text-[#e0e0ff] p-8 md:p-16">
+      <div>
         <HailMary />
       </div>
     </div>
