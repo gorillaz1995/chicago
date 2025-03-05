2bafd2a178fb4b4a5dd8e34009909d485397aa98
gorillaz1995 <104136116+gorillaz1995@users.noreply.github.com>
2025-03-05 18:46:52 +0200

    last push was a fail- be careful

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,10 +53,20 @@ const config: Config = {
         },
       },
       animation: {
+        shimmer: "shimmer 2s linear infinite",
         marquee: "marquee var(--duration) linear infinite",
         "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
       },
+
       keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
         marquee: {
           from: { transform: "translateX(0)" },
           to: { transform: "translateX(calc(-100% - var(--gap)))" },
