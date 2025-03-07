d284f70a0a51b42b052f2f5c498ff433ad1a419e
gorillaz1995 <104136116+gorillaz1995@users.noreply.github.com>
2025-03-07 22:03:01 +0200

    fine tune servicii x2

--- src/app/servicii/page.tsx
+++ src/app/servicii/page.tsx
@@ -26,7 +26,7 @@ function ServiciiPage() {
   return (
     <div style={{ position: "relative", width: "100%", height: "100%" }}>
       <HailMary />
-      <div className="gradient">
+      <div className="gradient1">
         <BentoGridThirdDemo />
       </div>
     </div>
